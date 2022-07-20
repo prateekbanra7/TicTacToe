af4d25cd17c0b6e59974fd7bc347d92ebe3c368e
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2022-07-20 23:57:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+package com.mygame;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program started...");
+        MyGame game=new MyGame();
+    }
+}
